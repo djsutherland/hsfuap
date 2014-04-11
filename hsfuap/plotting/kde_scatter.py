ae1e11d9aba5a67a8b5e481ada14e25219c6ca36
Dougal Sutherland <dougal@gmail.com>
2014-04-11 14:55:01 -0400

    kdescatter: optionally color with log density

--- hsfuap/plotting/kde_scatter.py
+++ hsfuap/plotting/kde_scatter.py
@@ -2,7 +2,7 @@ import numpy as np
 from scipy.stats import gaussian_kde
 
 
-def kdescatter(xs, ys, **kwargs):
+def kdescatter(xs, ys, log_color=False, **kwargs):
     import matplotlib.pyplot as plt
 
     kwargs.setdefault('linewidths', 0)
@@ -12,4 +12,6 @@ def kdescatter(xs, ys, **kwargs):
     dset = np.asarray([xs, ys])
     kde = gaussian_kde(dset)
     densities = kde.evaluate(dset)
+    if log_color:
+        np.log(densities, out=densities)
     plt.scatter(xs, ys, c=densities, **kwargs)
