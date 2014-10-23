726713c481ea6afd0ad6e11df7d7a60387845d4c
Dougal J. Sutherland <dougal@gmail.com>
2014-10-23 15:16:51 -0400

    make sdm.make_kernels friendlier about saving partial results

--- hsfuap/sdm/make_kernels.py
+++ hsfuap/sdm/make_kernels.py
@@ -4,22 +4,38 @@ import h5py
 import numpy as np
 import sdm
 
-def convert_file(inp, out, factors=[.25, 1, 4]):
-    for df in inp.keys():
-        if df == '_meta':
-            continue
-        for k in inp[df].keys():
-            divs = inp[df][k][()]
-            med = np.median(divs[np.triu_indices_from(divs)])
+def convert_file(in_file, out_file, factors=[.25, 1, 4]):
+    with h5py.File(in_file, 'r') as inp:
+        func_ks = [
+            (df, k)
+            for df, g in inp.iteritems() if df != '_meta'
+            for k in g.iterkeys()
+        ]
 
-            for factor in factors:
-                name = 'median * {}'.format(factor)
-                print '/'.join((df, k, name))
+    meds = {}
+    for df, k in func_ks:
+        with h5py.File(in_file, 'r') as inp:
+            divs = inp[df][k][()]
+
+        if df in meds:
+            med = meds[df]
+        else:
+            meds[df] = med = np.median(divs[np.triu_indices_from(divs)])
+
+        for factor in factors:
+            name = 'median * {}'.format(factor)
+            print '/'.join((df, k, name))
+
+            with h5py.File(out_file) as out:
                 g = out.require_group(df).require_group(k)
-                if name not in g:
-                    g[name] = sdm.sdm.make_km(divs, med * factor)
-                else:
+                if name in g:
                     print '\talready there'
+                    continue
+
+            km = sdm.sdm.make_km(divs, med * factor)
+            with h5py.File(out_file) as out:
+                out[df][k][name] = km
+
 
 def main():
     import argparse
@@ -29,8 +45,7 @@ def main():
     parser.add_argument('out_file')
     args = parser.parse_args()
 
-    with h5py.File(args.in_file, 'r') as inp, h5py.File(args.out_file) as out:
-        convert_file(inp, out, args.factors)
+    convert_file(args.in_file, args.out_file, args.factors)
 
 if __name__ == '__main__':
     main()
