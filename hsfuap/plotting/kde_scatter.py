320f16ab0300885405a678c9e4ef5c082ae8cf6e
Dougal J. Sutherland <dougal@gmail.com>
2014-10-23 15:17:30 -0400

    kdescatter updates from a while ago

--- hsfuap/plotting/kde_scatter.py
+++ hsfuap/plotting/kde_scatter.py
@@ -7,7 +7,8 @@ from sklearn.neighbors import KernelDensity
 __all__ = ['kdescatter']
 
 def kdescatter(xs, ys, log_color=False, atol=1e-4, rtol=1e-4,
-               n_jobs=1, n_samp_scaling=100, ax=None, **kwargs):
+               n_jobs=1, n_samp_scaling=100, n_samp_tuning=1000, ax=None,
+               **kwargs):
     if ax is None:
         import matplotlib.pyplot as plt
         ax = plt
@@ -17,13 +18,17 @@ def kdescatter(xs, ys, log_color=False, atol=1e-4, rtol=1e-4,
     kwargs.setdefault('cmap', 'winter')
 
     X = np.asarray([xs, ys]).T
-    samp_X = X[np.random.choice(X.shape[0], n_samp_scaling, replace=False)]
+    n = X.shape[0]
+    samp_X = X[np.random.choice(n, min(n_samp_scaling, n), replace=False)]
     median_sqdist = np.median(euclidean_distances(samp_X, squared=True))
     bws = np.logspace(-2, 2, num=10) * np.sqrt(median_sqdist)
     est = GridSearchCV(KernelDensity(), {'bandwidth': bws}, n_jobs=n_jobs)
-    est.fit(X)
+    est.fit(X[np.random.choice(n, min(n_samp_tuning, n), replace=False)])
+    bw = est.best_params_['bandwidth']
 
-    densities = est.best_estimator_.score_samples(X)
+    kde = KernelDensity(bandwidth=bw)
+    kde.fit(X)
+    densities = kde.score_samples(X)
     if not log_color:
         np.exp(densities, out=densities)
     ax.scatter(xs, ys, c=densities, **kwargs)
